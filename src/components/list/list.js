5073cef95093aa1d0ab9ded0d8ca2ce51ddbe909
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-06-18 10:01:25 +0200

    props and propsType

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -3,7 +3,7 @@ import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
 import './Table.css';
-
+import Table from './Table';
 class list extends React.Component {
     constructor() {
         super();
@@ -62,33 +62,10 @@ class list extends React.Component {
 
 
         return (
-            <div className='Table-container'>
-                <table className='Table'>
-                    <thead className="Table-head">
-                        <tr>
-                            <th>Cryptocurrency</th>
-                            <th>Price</th>
-                            <th>Market</th>
-                            <th>24H Change</th>
-                        </tr>
-                    </thead>
-                    <tbody className="Table-body">
-                        {currencies.map((currency) => (
-                            <tr key={currency.id}>
-                                <td>
-                                    <span className="Table-rank">{currency.rank}</span>
-                                    {currency.name}
-                                </td>
-                                <td>
-                                    <span className="Table-dollar">${currency.price}</span></td>
-                                <td>
-                                    <span className="Table-dollar">${currency.marketCap}</span></td>
-                                <td>{this.renderChangePercent(currency.percentChange24h)}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+           <Table 
+                currencies={currencies}
+                renderChangePercent={this.renderChangePercent}
+                />
         );
     }
 }
