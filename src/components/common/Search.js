d4dbc27148c48c7f75db17821b9ec2c66d9f9825
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-09-04 16:06:50 +0200

    loading mistake solved

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Loading from './Loading';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
-import Loading from '../common/Loading';
 import './Search.css';
 
 
 class Search extends React.Component{
-
     constructor(){
         super();
         
@@ -27,37 +26,42 @@ class Search extends React.Component{
         if(!searchQuery){
             return '';
         }
-        this.setState({loading:true});
+
+        this.setState({ loading: true });
+        
         fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
         .then(handleResponse)
         .then((result) => {
             console.log(result);
-            this.setState({ loading:false});
-
+        
+        this.setState({ loading: false});
         });
     }
 
     render() {
         const { loading } = this.state;
+
         return (
             <div className="Search">
                 <span className="Search-icon" />
-                    <input
+        
+                <input
                     className="Search-input"
                     type="text"
                     placeholder="Currency name"
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                />
+                    
                 {loading &&
                 <div className="Search-loading">
-                <loading 
-                width ="12px"
-                height ="12px"
+                <Loading 
+                    width ="12px"
+                    height ="12px"
                 />
                 </div>}
             </div>
         );
     }
 }
-
 
 export default Search;
